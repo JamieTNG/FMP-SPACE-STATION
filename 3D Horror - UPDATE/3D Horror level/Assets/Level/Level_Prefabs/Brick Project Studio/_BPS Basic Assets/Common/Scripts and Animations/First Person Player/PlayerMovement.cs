131c7e014b464973b5f955dbcfd48d202d839add
JamieTNG <114753199+JamieTNG@users.noreply.github.com>
2023-05-03 21:03:57 +0100

    commit

---
+++ 3D Horror - UPDATE/3D Horror level/Assets/Level/Level_Prefabs/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public CharacterController controller;
+
+        public float speed = 5f;
+        public float gravity = -15f;
+       
+        Vector3 velocity;
+        
+        bool isGrounded;
+        bool isCrouched;
+
+        void Update()
+        {
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+
+            if (Input.GetButtonDown("Crouch"))
+            {
+                isCrouched = true;
+                transform.localScale = new Vector3(1.3f, 0.8f, 1.3f);
+            }
+            else if(Input.GetButtonUp("Crouch"))
+            {
+                isCrouched = false;
+                transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+            }
+        }
+    }
+}
